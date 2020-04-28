b7c0be51eae48ea58c634cc10005ffd12009c0e3
Srivastav Nanduri <srivastav.nanduri.98@gmail.com>
2020-04-28 14:15:31 +0530

    Testing db connect

--- src/db.js
+++ src/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
+require('dotenv').config()
 var connection = mysql.createConnection({
-    host: 'hackathon.cgzeeefjondj.us-east-1.rds.amazonaws.com',
-    user: 'twilio',
-    password: 'SNr6RmcyVu8QKeL1z0Uc',
-    database: 'twilio'
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DATABASE
 });
     
     connection.connect(function(err) {
